079d25cca716654b12c354aa2c2b98c74794f5b7
paulboguta <paul.boguta@gmail.com>
2022-10-19 14:37:33 +0200

    setup dotenv

--- env.d.ts
+++ env.d.ts
@@ -1,10 +1,15 @@
 declare global {
-    namespace NodeJS {
-      interface ProcessEnv {
-        REACT_APP_API: string;
-      }
+  namespace NodeJS {
+    interface ProcessEnv {
+      REACT_APP_API: string;
+      REACT_APP_AUTHDOMAIN: string;
+      REACT_APP_DATABASE_URL: string;
+      REACT_APP_PROJECT_ID: string;
+      REACT_APP_STORAGE_BUCKET: string;
+      REACT_APP_MESSAGING_SENDER_ID: string;
+      REACT_APP_ID: string;
     }
   }
-  
-  export {}
-  
+}
+
+export {};
