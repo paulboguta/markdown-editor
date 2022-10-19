d5accd82ff258fb75f22cac8fe9adcbfaed5cb5c
paulboguta <paul.boguta@gmail.com>
2022-10-19 14:08:24 +0200

    hide api info

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,8 +4,8 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_API_AUTHDOMAIN,
-  databaseURL: process.env.REACT_APP_API_DATABASE_URL,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
