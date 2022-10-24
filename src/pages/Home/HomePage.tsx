25188e2737065c1d8668d51ce57b845e7aa6b947
paulboguta <paul.boguta@gmail.com>
2022-10-24 14:58:00 +0200

    add auth protection to home page

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,13 +1,26 @@
 import { MainLayout } from "pages/MainLayout/MainLayout";
 import { Menu } from "components/Menu/Menu";
 import { MenuContext } from "contexts/MenuContext";
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { Header } from "components/Header/Header";
+import { useSelector } from "react-redux";
+import { RootState } from "redux/store";
+import { useNavigate } from "react-router-dom";
 
 export const HomePage = () => {
   const [menuClicked, setMenuClicked] = useState<boolean>(false);
   const [showNewDocumentForm, setShowNewDocumentForm] =
     useState<boolean>(false);
+
+  // if there is user do nothing else navigate to signup
+  const { uid } = useSelector((state: RootState) => state.userReducer);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (uid.length < 1) {
+      navigate("/signup");
+    }
+  }, [navigate, uid]);
 
   const menuClickHandler = (): void => {
     setMenuClicked((prev: boolean) => !prev);
